77cbce6e68d147f890cbf5d4ddcbe8e2a28da239
Vivian Wang <vivian.emmenuel.wang@gmail.com>
2020-06-03 14:00:28 -0400

    Basic functions set with Algolia and API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
+import algoliasearch from 'algoliasearch/lite';
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  RefinementList,
+  Pagination,
+  Highlight,
+} from 'react-instantsearch-dom';
+import PropTypes from 'prop-types';
 import './App.css';
+
+const searchClient = algoliasearch('I3JU913ICM', '7bbc002b560942a366ca70ee403919d8');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <header className="header">
+        <h1 className="header-title">
+          TED Talks
+        </h1>
+        <p className="header-subtitle">
+          Look for the next talk to inspire you
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div className="container">
+        <InstantSearch searchClient={searchClient} indexName="talks">
+          <div className="search-panel">
+            <div className="search-panel__filters">
+              <RefinementList attribute="tags" />
+            </div>
+
+            <div className="search-panel__results">
+              <SearchBox
+                className="searchbox"
+                translations={{
+                  placeholder: '',
+                }}
+              />
+              <Hits hitComponent={Hit} />
+
+              <div className="pagination">
+                <Pagination />
+              </div>
+            </div>
+          </div>
+        </InstantSearch>
+      </div>
     </div>
   );
 }
+
+function Hit(props) {
+  return (
+    <div>
+      <div class="grid-container">
+        <div class="grid-x grid-margin-x">
+          <div className="hit-name cell small-4">
+            <Highlight attribute="name" hit={props.hit} />
+          </div>
+          <div className="hit-speakers cell small-4">
+            <Highlight attribute="speakers" hit={props.hit} />
+          </div>
+          <div className="hit-description cell small-4">{props.hit.description}</div>
+        </div>
+      </div>
+    </div>
+
+
+
+  );
+}
+
+Hit.propTypes = {
+  hit: PropTypes.object.isRequired,
+};
 
 export default App;
